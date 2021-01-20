b39ec137650bc32b9b08d2dd2832599acc3841e3
JuanMa Garrido <juanma.garrido@gmail.com>
2021-01-20 18:08:50 +0100

    Step 2.1 - Use the <Link> component

--- packages/my-first-theme/src/components/index.js
+++ packages/my-first-theme/src/components/index.js
@@ -1,11 +1,21 @@
+// File: /packages/my-first-theme/src/components/index.js
+
 import React from "react"
 import { connect } from "frontity"
+import Link from "@frontity/components/link"
 
 const Root = ({ state }) => {
   return (
     <>
-      <h1>Frontity Workshop</h1>
+      <h1>Hello Frontity</h1>
       <p>Current URL: {state.router.link}</p>
+      <nav>
+        <Link link="/">Home</Link>
+        <br />
+        <Link link="/page/2">More posts</Link>
+        <br />
+        <Link link="/about-us">About Us</Link>
+      </nav>
     </>
   )
 }
