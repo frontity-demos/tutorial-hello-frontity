eebb495ba16ede2d310bc838396aca662df5f3bc
JuanMa Garrido <juanma.garrido@gmail.com>
2021-01-20 17:55:21 +0100

    Step 1.4 - Connect the <Root> component to the state

--- packages/my-first-theme/src/components/index.js
+++ packages/my-first-theme/src/components/index.js
@@ -1,11 +1,13 @@
 import React from "react"
+import { connect } from "frontity"
 
-const Root = () => {
+const Root = ({ state }) => {
   return (
     <>
-      <h1>Hello Frontity</h1>
+      <h1>Frontity Workshop</h1>
+      <p>Current URL: {state.router.link}</p>
     </>
   )
 }
 
-export default Root
+export default connect(Root)
