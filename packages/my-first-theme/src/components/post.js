e43cb65d00fe8c3e56a7d1271c3b94f0a655210b
JuanMa Garrido <juanma.garrido@gmail.com>
2021-01-28 16:58:31 +0100

    Step 3.5 - Formatting the date

--- packages/my-first-theme/src/components/post.js
+++ packages/my-first-theme/src/components/post.js
@@ -1,17 +1,20 @@
 import React from "react"
 import { connect } from "frontity"
+import dayjs from "dayjs"
 
 const Post = ({ state }) => {
   const data = state.source.get(state.router.link)
   const post = state.source[data.type][data.id]
   const author = state.source.author[post.author]
 
+  const formattedDate = dayjs(post.date).format("DD MMMM YYYY")
+
   return (
     <div>
       <h2>{post.title.rendered}</h2>
       <p>
         <strong>Posted: </strong>
-        {post.date}
+        {formattedDate}
       </p>
       <p>
         <strong>Author: </strong>
