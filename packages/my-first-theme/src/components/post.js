efa2d467cc7b89a949b3102f340f52bfa8a21362
JuanMa Garrido <juanma.garrido@gmail.com>
2021-01-28 17:11:25 +0100

    Step 4.4 - Styling the post info

--- packages/my-first-theme/src/components/post.js
+++ packages/my-first-theme/src/components/post.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { connect } from "frontity"
+import { connect, styled } from "frontity"
 import dayjs from "dayjs"
 
 const Post = ({ state }) => {
@@ -12,17 +12,31 @@ const Post = ({ state }) => {
   return (
     <div>
       <h2>{post.title.rendered}</h2>
-      <p>
-        <strong>Posted: </strong>
-        {formattedDate}
-      </p>
-      <p>
-        <strong>Author: </strong>
-        {author.name}
-      </p>
+      <PostInfo>
+        <p>
+          <strong>Posted: </strong>
+          {formattedDate}
+        </p>
+        <p>
+          <strong>Author: </strong>
+          {author.name}
+        </p>
+      </PostInfo>
       <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
     </div>
   )
 }
+
+const PostInfo = styled.div`
+  background-image: linear-gradient(to right, #f4f4f4, #fff);
+  margin-bottom: 1em;
+  padding: 0.5em;
+  border-left: 4px solid lightseagreen;
+  font-size: 0.8em;
+
+  & > p {
+    margin: 0;
+  }
+`
 
 export default connect(Post)
